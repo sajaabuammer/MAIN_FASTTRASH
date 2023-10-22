56930e955d38543e89cea76ff7efe63f0b6e3126
sajaabuammer <sabua2@uic.edu>
2023-10-22 14:19:50 -0500

    NewUpdate_Saja_10/22_2:19PM

---
+++ NewLandfill_A4/Assets/Scripts/FogDisperse.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogDisperse : MonoBehaviour
+{
+    public GameObject A2DESTROY; // Assign the specific GameObject to monitor in the Unity Editor
+    public float fogDecreaseDuration = 1f; // Duration for the fog to dissipate completely
+    private float originalFogDensity; // Store the original fog density
+    private bool shouldDisperseFog = false;
+
+
+    void Start()
+    {
+        originalFogDensity = RenderSettings.fogDensity;
+    }
+
+    void Update()
+    {
+        if (A2DESTROY == null && !shouldDisperseFog)
+        {
+            shouldDisperseFog = true;
+            StartCoroutine(DecreaseFogDensity());
+        }
+    }
+    IEnumerator DecreaseFogDensity()
+    {
+        float elapsedTime = 0f;
+        float currentFogDensity = RenderSettings.fogDensity;
+
+        while (elapsedTime < fogDecreaseDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime / fogDecreaseDuration);
+            RenderSettings.fogDensity = Mathf.Lerp(currentFogDensity, 0f, t);
+            yield return null;
+        }
+
+        // Ensure that fog is completely removed at the end
+        RenderSettings.fogDensity = 0f;
+        RenderSettings.fog = false;
+    }
+}
---
+++ NewLandfill_A4/Assets/Scripts/FogIncrease.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogIncrease : MonoBehaviour
+{
+    public GameObject LetterA;
+    public float targetFogDensity = 0.04f; // Set the target fog density in the Unity Editor
+    public float fogIncreaseDuration = 10f; // Duration for the fog to reach full density
+    private float originalFogDensity; // Store the original fog density
+    private bool shouldIncreaseFog = false; // Flag to determine whether to increase the fog density
+
+    void Start()
+    {
+        originalFogDensity = RenderSettings.fogDensity;
+    }
+
+    void Update()
+    {
+        if (LetterA == null && !shouldIncreaseFog)
+        {
+            shouldIncreaseFog = true;
+            StartCoroutine(IncreaseFogDensity());
+        }
+    }
+
+    IEnumerator IncreaseFogDensity()
+    {
+        float elapsedTime = 0f;
+        float currentFogDensity = RenderSettings.fogDensity;
+
+        while (elapsedTime < fogIncreaseDuration && currentFogDensity < targetFogDensity)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime / fogIncreaseDuration);
+            RenderSettings.fogDensity = Mathf.Lerp(currentFogDensity, targetFogDensity, t);
+            yield return null;
+        }
+    }
+}
