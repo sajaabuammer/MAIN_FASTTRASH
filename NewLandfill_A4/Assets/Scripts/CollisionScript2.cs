9d2760ad142df05c3e585225c9836a8557527b88
SalmaAbdelhady25 <sabde4@uic.edu>
2023-10-21 21:39:23 -0500

    Update (Salma)

---
+++ NewLandfill_A4/Assets/Scripts/CollisionScript2.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript2 : MonoBehaviour
+{
+    public GameObject Ground; // Reference to the ground object.
+
+    public GameObject BigPile9;
+    public GameObject BigPile10;
+    public GameObject BigPile11;
+    public GameObject BigPile12;
+    public GameObject BigPile13;
+    public GameObject BigPile14;
+    public GameObject BigPile15;
+    public GameObject BigPile16;
+
+    public float fallSpeed = 1.0f; // Adjust the fall speed as needed.
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            MoveObjectsUnderGround(BigPile9);
+            MoveObjectsUnderGround(BigPile10);
+            MoveObjectsUnderGround(BigPile11);
+            MoveObjectsUnderGround(BigPile12);
+            MoveObjectsUnderGround(BigPile13);
+            MoveObjectsUnderGround(BigPile14);
+            MoveObjectsUnderGround(BigPile15);
+            MoveObjectsUnderGround(BigPile16);
+        }
+    }
+
+    void MoveObjectsUnderGround(GameObject obj)
+    {
+        if (obj != null && Ground != null)
+        {
+            StartCoroutine(FallObject(obj));
+        }
+    }
+
+    IEnumerator FallObject(GameObject obj)
+    {
+        Vector3 startPosition = obj.transform.position;
+        Vector3 targetPosition = obj.transform.position;
+        targetPosition.y = -17f; // Adjust this value based on your ground's height.
+
+        float journeyLength = Mathf.Abs(targetPosition.y - startPosition.y);
+        float startTime = Time.time;
+        float distanceCovered = 0;
+        float journeyFraction = 0;
+
+        while (journeyFraction < 1)
+        {
+            distanceCovered = (Time.time - startTime) * fallSpeed;
+            journeyFraction = distanceCovered / journeyLength;
+            obj.transform.position = Vector3.Lerp(startPosition, targetPosition, journeyFraction);
+            yield return null; // Wait for the next frame.
+        }
+    }
+}
+
+
+
