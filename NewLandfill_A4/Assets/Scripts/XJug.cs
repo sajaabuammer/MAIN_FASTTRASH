eb072e87275dfc6afa5ab58b5d2876ee52f20d24
Iriam Garcia <igarc9@uic.edu>
2023-10-27 19:10:05 -0500

    10/27 7pm

---
+++ NewLandfill_A4/Assets/Scripts/XJug.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XJug : MonoBehaviour
+{
+    public GameObject objectToAppear; // The object to appear upon trigger
+
+    private void Start()
+    {
+        objectToAppear.SetActive(false); // Ensure the object is initially disabled
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            objectToAppear.SetActive(true); // Enable the object when the trigger is entered by the player
+        }
+    }
+}
