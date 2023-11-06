1b6d4747f3e016ccb8dc4765732727dc66aadebe
sajaabuammer <sabua2@uic.edu>
2023-11-06 02:11:30 -0600

    FINAL FINAL FINAL FINAL FINAL WOOOOOOOOOOOOOOO
    WE'RE DONE FOREVERRRRR!!!!!!!!

--- NewLandfill_A4/Assets/Scripts/ClothesFalling4.cs
+++ NewLandfill_A4/Assets/Scripts/ClothesFalling4.cs
@@ -9,16 +9,11 @@ public class ClothesFalling4 : MonoBehaviour
     // Reference to Textile1 in the Inspector.
     public GameObject Boat1;
     public GameObject Boat2;
-    public GameObject Boat3;
-    public GameObject POSTIVE1;
-    public GameObject POSTIVE2;
-    public GameObject POSTIVE3;
-    public GameObject POSTIVE4;
-    public GameObject POSTIVE5;
-    public GameObject POSTIVE6;
     public GameObject WaterPlatform;
     public AudioClip mySound;
     public Material newSkybox;
+    public GameObject placetextile;
+    public GameObject Spin;
     void Start()
     {
         // Get the Rigidbody component attached to Cloth1.
@@ -33,13 +28,8 @@ public class ClothesFalling4 : MonoBehaviour
         WaterPlatform.SetActive(false);
         Boat1.SetActive(false);
         Boat2.SetActive(false);
-        Boat3.SetActive(false);
-        POSTIVE1.SetActive(false);
-        POSTIVE2.SetActive(false);
-        POSTIVE3.SetActive(false);
-        POSTIVE4.SetActive(false);
-        POSTIVE5.SetActive(false);
-        POSTIVE6.SetActive(false);
+        placetextile.SetActive(true);
+        Spin.SetActive(true);
 
     }
 
@@ -50,18 +40,14 @@ public class ClothesFalling4 : MonoBehaviour
             // Enable gravity to make Cloth1 fall with normal speed.
             rb.useGravity = true;
             WaterPlatform.SetActive(true);
-            Boat1.SetActive(true);
-            Boat2.SetActive(true);
-            Boat3.SetActive(true);
-            POSTIVE1.SetActive(true);
-            POSTIVE2.SetActive(true);
-            POSTIVE3.SetActive(true);
-            POSTIVE4.SetActive(true);
-            POSTIVE5.SetActive(true);
-            POSTIVE6.SetActive(true);
             GetComponent<AudioSource>().PlayOneShot(mySound);
             RenderSettings.skybox = newSkybox;
-          
+            Boat1.SetActive(true);
+            Boat2.SetActive(true);
+            placetextile.SetActive(false);
+            Spin.SetActive(false);
+
+
         }
     }
 }
