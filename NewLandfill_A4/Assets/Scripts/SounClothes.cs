b30dc8e8e3047ac9448e7183f2ba2ced2dc626a6
Iriam Garcia <igarc9@uic.edu>
2023-11-05 23:30:32 -0600

    FInal

---
+++ NewLandfill_A4/Assets/Scripts/SounClothes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SounClothes : MonoBehaviour
+{
+    public AudioClip collisionSound;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collisionSound != null)
+        {
+            // Check if the relative velocity of the collision is strong enough
+            if (collision.relativeVelocity.magnitude > 8f)
+            {
+                audioSource.PlayOneShot(collisionSound);
+            }
+        }
+		
+    }
+}
